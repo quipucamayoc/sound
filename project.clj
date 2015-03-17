d8ca60b8964997147b55b86febd430b074294ffb
Boris Kourtoukov <boriskourt@gmail.com>
2015-03-17 02:44:05 -0400

    Working on tone.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject quipucamayoc "0.2.1"
+(defproject quipucamayoc "0.2.2"
             :description "Quipucamayoc Core *&* Sound"
             :url "http://quipucamayoc.com/"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-            :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
-                           [quil "2.2.4"]
+            :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+                           [quil "2.2.5" :exclusions [org.clojure/clojure]]
                            [overtone "0.9.1"]
                            [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
@@ -21,6 +21,17 @@
             :resource-paths ["resources"]
 
             :plugins [[lein-pprint "1.1.2"]
-                      [lein-gorilla "0.3.3"]
-                      [lein-ancient "0.6.0-SNAPSHOT"]
-                      [lein-marginalia "0.8.0"]])
+                      [lein-gorilla "0.3.5-SNAPSHOT"]
+                      [lein-bikeshed "0.2.0"]
+                      [lein-kibit "0.0.8"]
+                      [lein-hiera "0.9.0"]
+                      [jonase/eastwood "0.2.1"]
+                      [lein-ancient "0.6.5"]
+                      [lein-marginalia "0.8.0"]]
+
+            :hera {:path "target/ns-hierarchy.png"
+              :vertical true
+               :show-external true
+                :cluster-depth 0
+                 :trim-ns-prefix true
+                  :ignore-ns #{}})
--- src/quipucamayoc/tone.clj
+++ src/quipucamayoc/tone.clj
@@ -11,6 +11,92 @@
 
 (defonce sc-server (boot-internal-server))
 
+;; ## Sample Instruments
+;; ### Guitar Pluck
+(defonce bs1 (load-sample "assets/guitar_pluck/bs1.wav"))
+(definst play-bs1 [amp 1]
+         (* (play-buf 1 bs1) amp))
+(def static-bs1 (partial play-bs1 :amp 1))
+
+(defonce bs2 (load-sample "assets/guitar_pluck/bs2.wav"))
+(definst play-bs2 [amp 1]
+         (* (play-buf 1 bs2) amp))
+(def static-bs2 (partial play-bs2 :amp 1))
+
+(defonce bs3 (load-sample "assets/guitar_pluck/bs3.wav"))
+(definst play-bs3 [amp 1]
+         (* (play-buf 1 bs3) amp))
+(def static-bs3 (partial play-bs3 :amp 1))
+
+(defonce bs4 (load-sample "assets/guitar_pluck/bs4.wav"))
+(definst play-bs4 [amp 1]
+         (* (play-buf 1 bs4) amp))
+(def static-bs4 (partial play-bs4 :amp 1))
+
+(defonce bs5 (load-sample "assets/guitar_pluck/bs5.wav"))
+(definst play-bs5 [amp 1]
+         (* (play-buf 1 bs5) amp))
+(def static-bs5 (partial play-bs5 :amp 1))
+
+(defonce bs6 (load-sample "assets/guitar_pluck/bs6.wav"))
+(definst play-bs6 [amp 1]
+         (* (play-buf 1 bs6) amp))
+(def static-bs6 (partial play-bs6 :amp 1))
+
+(defonce bs7 (load-sample "assets/guitar_pluck/bs7.wav"))
+(definst play-bs7 [amp 1]
+         (* (play-buf 1 bs7) amp))
+(def static-bs7 (partial play-bs7 :amp 1))
+
+(defonce bs8 (load-sample "assets/guitar_pluck/bs8.wav"))
+(definst play-bs8 [amp 1]
+         (* (play-buf 1 bs8) amp))
+(def static-bs8 (partial play-bs8 :amp 1))
+
+(defonce bs9 (load-sample "assets/guitar_pluck/bs9.wav"))
+(definst play-bs9 [amp 1]
+         (* (play-buf 1 bs9) amp))
+(def static-bs9 (partial play-bs9 :amp 1))
+
+;; ### Toy Piano
+
+(defonce tp1 (load-sample "assets/toy_piano/125623__noisecollector__pianohit1.wav"))
+(definst play-tp1 [amp 0.5]
+         (* (play-buf 1 tp1) amp))
+(def static-tp1 (partial play-tp1 :amp 0.5))
+
+(defonce tp2 (load-sample "assets/toy_piano/125624__noisecollector__pianohit2.wav"))
+(definst play-tp2 [amp 0.5]
+         (* (play-buf 1 tp2) amp))
+(def static-tp2 (partial play-tp2 :amp 0.5))
+
+(defonce tp3 (load-sample "assets/toy_piano/125625__noisecollector__pianohit3.wav"))
+(definst play-tp3 [amp 0.5]
+         (* (play-buf 1 tp3) amp))
+(def static-tp3 (partial play-tp3 :amp 0.5))
+
+(defonce tp4 (load-sample "assets/toy_piano/125627__noisecollector__pianonewhit2.wav"))
+(definst play-tp4 [amp 0.5]
+         (* (play-buf 1 tp4) amp))
+(def static-tp4 (partial play-tp4 :amp 0.5))
+
+(defonce tp5 (load-sample "assets/toy_piano/125628__noisecollector__pianonewhit3.wav"))
+(definst play-tp5 [amp 0.5]
+         (* (play-buf 1 tp5) amp))
+(def static-tp5 (partial play-tp5 :amp 0.5))
+
+(defonce tp6 (load-sample "assets/toy_piano/125629__noisecollector__pianonewhit4.wav"))
+(definst play-tp6 [amp 0.5]
+         (* (play-buf 1 tp6) amp))
+(def static-tp6 (partial play-tp6 :amp 0.5))
+
+(defonce tp7 (load-sample "assets/toy_piano/125626__noisecollector__pianonewhit1.wav"))
+(definst play-tp7 [amp 0.5]
+         (* (play-buf 1 tp7) amp))
+(def static-tp7 (partial play-tp7 :amp 0.5))
+
+
+
 ;; ## Sound Helpers
 
 (defn playat [time delta offset]
@@ -31,26 +117,26 @@
 ;; #### Large
 
 (defmethod axis-trigger :large [msg]
-  (case (:sensor msg)
-    :x (guitar-pick md-guitar 1 5)
-    :y (guitar-pick md-guitar 1 4)
-    :z (guitar-pick md-guitar 1 3)))
+    (case (:sensor msg)
+      :x (guitar-pick lg-guitar 5 7)
+      :y (guitar-pick lg-guitar 3 5)
+      :z (guitar-pick lg-guitar 1 3)))
 
 ;; #### Medium
 
 (defmethod axis-trigger :medium [msg]
-  (case (:sensor msg)
-    :x (guitar-pick md-guitar 3 6)
-    :y (guitar-pick md-guitar 3 5)
-    :z (guitar-pick md-guitar 3 4)))
+    (case (:sensor msg)
+      :x (guitar-pick md-guitar 5 7)
+      :y (guitar-pick md-guitar 3 5)
+      :z (guitar-pick md-guitar 1 3)))
 
 ;; #### Small
 
 (defmethod axis-trigger :small [msg]
-  (case (:sensor msg)
-    :x (guitar-pick sm-guitar 5 7)
-    :y (guitar-pick sm-guitar 5 6)
-    :z (guitar-pick sm-guitar 5 5)))
+    (case (:sensor msg)
+      :x (static-bs6)
+      :y (static-bs8)
+      :z (static-bs9)))
 
 ;; ## Event distributor
 
@@ -66,7 +152,7 @@
 (defn init
   "Sets instruments to their initial state, starts listening for events."
   []
-  (ctl lg-guitar :pre-amp 4 :distort 0.12 :noise-amp 0.82
+  (ctl lg-guitar :pre-amp 4 :distort 0.14 :noise-amp 0.82
        :lp-freq 3400 :lp-rq 4.5
        :rvb-mix 0.01 :rvb-room 0.01 :rvb-damp 0.5)
 
@@ -74,7 +160,7 @@
        :lp-freq 3400 :lp-rq 2.5
        :rvb-mix 0.01 :rvb-room 0.01 :rvb-damp 0.5)
 
-  (ctl sm-guitar :pre-amp 4 :distort 0.12 :noise-amp 0.82
+  (ctl sm-guitar :pre-amp 4 :distort 0.10 :noise-amp 0.82
        :lp-freq 3400 :lp-rq 1.5
        :rvb-mix 0.01 :rvb-room 0.01 :rvb-damp 0.5)
 
