d77dd8b7cb435dd3bfb0c1fb958ebd17609848fe
Boris Kourtoukov <boriskourt@gmail.com>
2015-10-13 14:42:53 -0400

    med

--- src/sound/comm.clj
+++ src/sound/comm.clj
@@ -335,10 +335,10 @@
       (doall (mapv (fn [[id data]]
                      (when-let [t (not (or (nil? (last (:type data)))
                                            (false? (last (:type data)))))]
-                       (case (abs (last (:type data)))
-                         0.0 (do (axis-mapped-no-touch {id data} -250 250 :sample-blend :thunder-storm))
-                         1.0 (do (axis-mapped-no-touch-upper-sensor {id data} -250 250 :sample-blend :thunder-storm))
-                         2.0 (do (axis-mapped {id data} -250 250 :sample-blend :thunder-storm))
+                       (case (last (:type data))
+                         0.0 (do (pprint (last (:type data))) (axis-mapped-no-touch {id data} -250 250 :sample-blend :thunder-storm))
+                         1.0 (do (pprint (last (:type data))) (axis-mapped-no-touch-upper-sensor {id data} -250 250 :sample-blend :thunder-storm))
+                         2.0 (do (pprint (last (:type data))) (axis-mapped {id data} -250 250 :sample-blend :thunder-storm))
                          (pprint data)))) vmap)))
 
     (comment (fn [& args]
